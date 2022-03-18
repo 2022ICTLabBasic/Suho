eff248e8a08ef5b0f724d4a476356ae8b33f4ac3
choi su ho <csw020106@naver.com>
2022-03-18 23:52:08 +0900

    고양이

---
+++ 백준_입출력과 사칙연산_1단계/고양이.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+// 아래 예제와 같이 고양이를 출력하시오.
+
+int main(void)
+{
+    printf("\\    /\\\n");
+    printf(" )  ( \')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|");
+    return 0;
+}
