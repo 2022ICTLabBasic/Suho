ebe05d2ad3d4bdf80759bd53167cc86b29844070
choi su ho <csw020106@naver.com>
2022-03-19 23:56:30 +0900

    두 수 비교하기 1단계

---
+++ 백준_if문_2단계/두 수 비교하기.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+// 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
+
+int main(void)
+{
+    // 방법 1
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a > b)
+    {
+        printf(">");
+    }
+    else if(a < b)
+    {
+        printf("<");
+    }
+    else
+    {
+        printf("==");
+    }
+
+    // 방법 2
+    (a > b) ? printf(">") : (a < b) ? printf("<") : printf("==");
+    return 0;
+}
