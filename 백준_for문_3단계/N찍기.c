a672ecb10f5edf1896f3492c36c2479e943adeb7
choi su ho <csw020106@naver.com>
2022-03-20 17:21:42 +0900

    N 찍기 5단계

---
+++ 백준_for문_3단계/N찍기.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+// 자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+
+int main(void)
+{
+    // 방법 1
+    int n;
+    scanf("%d",&n);
+    for(int i = 1; i <= n; i++)
+    {
+        printf("%d\n",i);
+    }
+    // 방법 2
+    int i = 1;
+    while (i <= n)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+    
+    return 0;
+}
