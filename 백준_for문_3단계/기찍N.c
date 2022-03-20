e5aa9a884cd6dc4decbbb31f8af81eb26d9a574f
choi su ho <csw020106@naver.com>
2022-03-20 17:21:58 +0900

    기찍 N 6단계

---
+++ 백준_for문_3단계/기찍N.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+// 자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+
+int main(void)
+{
+    // 방법 1
+    int n;
+    scanf("%d",&n);
+    for(int i = n; i >= 1; i--)
+    {
+        printf("%d\n",i);
+    }
+    // 방법 2
+    int i = n;
+    while (i >= 1)
+    {
+        printf("%d\n",i);
+        i--;
+    }
+    
+    return 0;
+}
